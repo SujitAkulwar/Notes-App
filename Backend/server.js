685871637fbaddbb46cd31637fcb90d420aefc74
sujit akulwar <85801946+SujitAkulwar@users.noreply.github.com>
2026-03-11 04:36:59 +0530

    vercel setup for backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,24 @@
 import "dotenv/config";
-import express, { json } from 'express';
-import { connect } from 'mongoose';
-import cors from 'cors';
-import errorHandler from './src/middleware/middleware.js';
-import noteRoutes from './src/routers/Routes.js';
+import express, { json } from "express";
+import { connect } from "mongoose";
+import cors from "cors";
+
+import errorHandler from "./src/middleware/middleware.js";
+import noteRoutes from "./src/routers/Routes.js";
 
 const app = express();
+
 app.use(cors());
 app.use(json());
 
 // Routes
-app.use('/api/notes', noteRoutes);
+app.use("/api/notes", noteRoutes);
 
 app.use(errorHandler);
 
+// MongoDB connection
 connect(process.env.mongodb_url)
-  .then(() => app.listen(5000, () => console.log("Server running on port 5000")))
-  .catch(err => console.log(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+export default app;
